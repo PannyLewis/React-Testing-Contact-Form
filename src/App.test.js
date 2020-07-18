e513d6a0f49814ca2aa8e8cda4d0a9562e28b29f
Panny Lewis <pannylewis@gmail.com>
2020-07-18 01:44:29 -0500

    App.test.js: test the submit button.

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
+});
+
+test("submits", () => {
+  const onSubmit = jest.fn();
+  const { getByTestId } = render(<ContactForm onSubmit={onSubmit} />);
+  fireEvent.submit(getByTestId("form"));
+  expect(onSubmit).toHaveBeenCalled();
 });
